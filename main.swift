a8a4c5df91767d901e625bbff03d8c51afb20398
nianny <75959161+nianny@users.noreply.github.com>
2021-09-08 11:52:07 +0800

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,1 +1,17 @@
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      ScrollView{
+        ForEach(0...200){ number in
+             Text(number)
+           }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
